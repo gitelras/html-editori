3a63549a9e20721129c27efd1b257d3031069e8f
gitelras <ella.rasalo@helsinki.fi>
2024-03-09 22:55:32 +0200

    tehtävä 6

--- laskarit/viikko2/unicafe/src/maksukortti.py
+++ laskarit/viikko2/unicafe/src/maksukortti.py
@@ -4,9 +4,14 @@ class Maksukortti:
         self.saldo = saldo
 
     def lataa_rahaa(self, maara):
+        if maara < 0:
+            return
         self.saldo += maara
 
     def ota_rahaa(self, maara):
+        if maara < 0:
+            return
+
         if self.saldo < maara:
             return False
 
--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,38 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_korti_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 35.0)
+    
+    def test_kortille_ei_voi_ladata_neg_rahaa(self):
+        self.maksukortti.lataa_rahaa(-2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_ota_rahaa_ei_vie_saldoa_negatiiviseksi(self):
+        self.maksukortti.ota_rahaa(-2500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+    
+    def test_ota_rahaa_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(250)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 7.50)
+    
+    def test_ota_rahaa_palautttaa_True_jos_rahat_riittavat(self):
+
+        self.assertEqual(self.maksukortti.ota_rahaa(250), True)
+    
+    def test_ota_rahaa_palautttaa_True_jos_rahat_riittavat(self):
+
+        self.assertEqual(self.maksukortti.ota_rahaa(250), True)
+
+    def test_ota_rahaa_palautttaa_False_jos_rahat_ei_riita(self):
+
+        self.assertEqual(self.maksukortti.ota_rahaa(2500), False)
