8abd2df08ac159de846b8dbac4fd0fb0333ef71c
gitelras <ella.rasalo@helsinki.fi>
2024-03-22 23:04:03 +0200

    create add_child

--- src/services/html_builder.py
+++ src/services/html_builder.py
@@ -1,9 +1,14 @@
 from repositories.html_repository import Save
+from entities.node import Node
 
 class HTML_builder:
     def __init__(self):
         self.text = ""
-    
+        root = Node()
+
+    def add_child(self, parent):
+        parent.children.append(Node())
+
     def import_text(self, text):
         self.text = text
         print(self.text)
